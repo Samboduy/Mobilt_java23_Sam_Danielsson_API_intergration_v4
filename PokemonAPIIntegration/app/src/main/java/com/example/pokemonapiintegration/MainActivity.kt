4a7cab42d5f561e1e9ecde627acb9d322d0bd394
Samboduy <samdanielsson000@gmail.com>
2024-09-11 21:31:49 +0200

    push

---
+++ PokemonAPIIntegration/app/src/main/java/com/example/pokemonapiintegration/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.pokemonapiintegration
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val pokemon: Intent =Intent(this,SearchPokemonActivity::class.java)
+        startActivity(pokemon)
+
+
+    }
+}
